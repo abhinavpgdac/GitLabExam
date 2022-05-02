c2763a67a1ba2d3a63abd5d442017f28fe8ddae7
Abhinav Mishra <101630308+abhinavpgdac@users.noreply.github.com>
2022-05-02 20:28:50 +0530

    Add files via upload

---
+++ Pract3.java
@@ -0,0 +1,14 @@
+package Assignment1;
+
+//write a program that defines two variables and print their sum .
+
+public class Pract3 {
+
+	public static void main(String[] args) {
+		int a=25;
+		int b=25;
+		System.out.println("Sum of a & b = " +(a+b));
+				
+	}
+
+}
